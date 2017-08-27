ab8737d86e2f2b41d270be3bcac75597c1ba97b6
Sander Mertens <sander.mertens8@gmail.com>
2017-08-27 04:32:59 -0700

    Initial commit

---
+++ include/sh.h
@@ -0,0 +1,23 @@
+/* sh.h
+ *
+ * This is the main package file. Include this file in other projects.
+ * Only modify inside the header-end and body-end sections.
+ */
+
+#ifndef DRIVER_TOOL_SH_H
+#define DRIVER_TOOL_SH_H
+
+#include <corto/corto.h>
+#include <driver/tool/sh/_project.h>
+#include <corto/c/c.h>
+
+/* $header() */
+/* Definitions that are required by package headers (native types) go here. */
+/* $end */
+
+/* $body() */
+/* Definitions here that need your package headers go here. */
+/* $end */
+
+#endif
+
